3ff1ef5c25b1ff5b2cb13fea6b1586f2a2d0203a
igorstankovic112 <100700941+igorstankovic112@users.noreply.github.com>
2023-03-12 23:21:22 +0100

    commita

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let url = window.location.pathname;
-// upis json fajla u local storage
 
+
+// upis json fajla u local storage
 window.onload = function(){
     ajaxCallback("books",function(result){
         toLocalStorage("books",result);
@@ -65,7 +66,7 @@ function ajaxCallback(file, result){
         }
     });
 }
-
+var numOfBooks = 0;
 var page = document.getElementById("row");
 function print(data){
     //console.log(data);
@@ -77,14 +78,14 @@ function print(data){
     if(checkBookHtml()) data = newBooks(data);
     if(checkBookHtml()) data = sortiranje(data);
     if(checkBookHtml()) data = freeShip(data);
-
+    var picture = [];
     let brojac = 1;
     let html = `<div class="card-deck">`;
     data.forEach(book => {
-
-        html +=`<div class="card">
+        numOfBooks++;
+        html +=`<div class="card" onclick="createModal(${book.id})">
                     <div class="px-0">
-                    <img src="${book.img.src}" class="img-fluid" alt="${book.img.alt}">
+                    <button  data-toggle="modal" data-target="#modal" class="bookPics"><img src="${book.img.src}" class="img-fluid picsBut" alt="${book.img.alt}"></button>
                     </div>
                     <div class="card-body">
                         <div class="p-3">
@@ -92,7 +93,7 @@ function print(data){
                             <p class="free">First publish in: ${book.year}.</p>
                             <p class="price h3">${book.price.new}$ <s>${book.price.old}$</s></p>
                             <p class="free">${book.shipping}</p>
-                            <a id="buyButton" class="btn btn-primary">Add to Cart</a>
+                            
                         </div>
                     </div>
                 </div>`
@@ -103,10 +104,142 @@ function print(data){
         }
         else brojac++;
         
+        
     });
+    
+    
+    console.log(numOfBooks);
     html +=`</div>`
+    
     page.innerHTML = html;
+    //picture = document.getElementsByClassName("picsBut");
+    
+    
+   
+    /*for(let i = 0; i < picture.length;i++){
+        picture[i].addEventListener("click",function(){
+            let pcVal = picture[i].src;
+            var srcRow = [];
+            data.forEach(book => {
+                
+                srcRow.push(book.img.src);
+                //console.log(srcRow);
+            });
+
+            if(srcRow[i] == pcVal) console.log("uspelo");
+
+
+            for (let index = 0; index < srcRow.length; index++) {
+                
+                var rowCurrentBooks = [];
+                rowCurrentBooks = data.filter(element => element.img.src == srcRow[index])
+                console.log(rowCurrentBooks);
+                printModal(data,srcRow[index])
+            }
+            
+            //console.log(srcRow);
+            
+            console.log(pcVal)
+            console.log(srcRow[i])
+            let modalNiz = [];
+            modalNiz = data.filter(element => element.img.src == pcVal)
+            console.log(modalNiz);
+            
+            //console.log(data);
+            printModal(data,pcVal)
+        });
+    }*/
 };
+/*var allModals = document.getElementById("modal");
+function printModal(books,src){
+    let html = "";
+    let row = [];
+    row = books.filter(element => element.img.src == src);
+    console.log(books.filter(element => element.img.src == "assets/images/fellowship.jpg"))
+    console.log(row);
+    if(books.filter(element => element.img.src == src)){
+    console.log("1");
+    row.forEach(element =>{
+        html =
+        `<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content" id="modals">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Buy a Book</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="card card-modal">
+                <div class="px-0">
+                <button data-toggle="modal" data-target="#modal" class="bookPics"><img src="${element.img.src}" class="img-fluid " alt="${element.img.alt}"></button>
+                </div>
+                <div class="card-body">
+                    <div class="p-3">
+                        <h4>${element.name}</h4>
+                        <p class="free">First publish in: ${element.year}.</p>
+                        <p class="price h3">${element.price.new}$ <s>${element.price.old}$</s></p>
+                        <p class="free">${element.shipping}</p>
+                        
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <a id="buyButton" class="btn btn-primary">Add to Cart</a>
+                <input type="number" name="add" id="add-to-cart-value" min="1" value="1" class="col-3 m-2">
+            </div>
+                </div>
+        </div>`
+    })
+       
+        
+    allModals.innerHTML = html;
+}
+}
+*/
+
+function createModal(id){
+    let modalbooks = getFromLocalStorage("books");
+    let html = "";
+    console.log(id);
+    for(book of modalbooks){
+        if(book.id == id){
+            html = `<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                        <div class="modal-content" id="modals">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Buy a Book</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="card card-modal">
+                                    <div class="px-0 modalPic">
+                                        <button data-toggle="modal" data-target="#modal" class="bookPics"><img src="${book.img.src}" class="img-fluid modalPic" alt="${book.img.alt}"></button>
+                                    </div>
+                                    <div class="card-body modal-bod">
+                                        <div class="p-3">
+                                            <h4>${book.name}</h4>
+                                            <p><p class="bold">Description:</p> ${book.description}</p>
+                                            <p class="price h3">${book.price.new}$ <s>${book.price.old}$</s></p>
+                                            <p><p class="bold">Stock:</p> ${book.stock}</p>
+                                            <p class="free">${book.shipping}</p>
+                                            <a id="buyButton" class="btn btn-primary">Add to Cart</a>
+                                            <input type="number" name="add" id="add-to-cart-value" min="1" value="1" class="col-3 m-2">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>`
+
+        }
+    }
+    document.getElementById("modal").innerHTML = html;
+    //$("#modal").append(html);
+}
 
 
 function latestBooks(books){
@@ -291,9 +424,9 @@ function authorFilter(books){
     return authors;
 }
 
-
+//form
+if(checkIndexHtml()){
 let regexName = /^[A-ZČĆŠĐŽ][a-zčćšđž]{2,15}(\s[A-ZČĆŠĐŽ][a-zčćšđž]{2,15})?(\s[A-ZČĆŠĐŽ][a-zčćšđž]{2,20})\s*$/;
-
 let textName = document.getElementById('name');
 fieldName = document.getElementById('nameErorr');
 textName.addEventListener('blur', checkName);
@@ -395,3 +528,4 @@ for (let i = 0; i < portfolio.length; i++) {
         portfolio[i].classList.remove("effect");
     });
 }
+};
